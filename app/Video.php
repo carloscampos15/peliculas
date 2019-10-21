e56c8fea59eeab2cd8f5d907ac7284ef4ffbdd1d
juanMoraa <juan.moraa@gmail.com>
2019-10-21 10:04:19 -0500

    creacion tabla videos y migracion

---
+++ app/Video.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Video extends Model
+{
+     
+    protected $fillable = [
+        'nombre', 'duracion', 'descripcion', 'categoria_id','autor_id','calificacion', 'nro_vistas',
+    ];
+}
---
+++ database/migrations/2019_10_21_144812_create_videos_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVideosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('videos', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nombre');
+            $table->string('duracion');
+            $table->string('descripcion');
+            $table->unsignedBigInteger('categoria_id');
+            $table->unsignedBigInteger('autor_id');
+            $table->foreign('categoria_id')->references('id')->on('categorias');
+            $table->foreign('autor_id')->references('id')->on('user');
+            $table->integer('calificacion');
+            $table->integer('descripcion');
+
+
+
+'nombre', 'duracion', 'descripcion', 'categoria_id','autor_id','calificacion', 'nro_vistas',
+
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('videos');
+    }
+}
